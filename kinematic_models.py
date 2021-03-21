20a73382d6c8ff8e6accb5e68781b2a9beae1150
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-03-21 14:15:14 -0500

    changed variable names to be consistent with dynamic model

--- kinematic_models.py
+++ kinematic_models.py
@@ -11,23 +11,25 @@ class KinematicBicycle:
     def __init__(self, lf, lr):
         # model constants
         self.lf = lf  # distance from front axle to tracking point
-        self.lr = lr  # distance from rear axle to tracking point
+        self.lr = lr  # distance from rear aXle to tracking point
 
         # dynamic parameters
-        self.x = 0. # position
-        self.y = 0.  # position
-        self.yaw = 0.  # heading angle
-        self.v = 0.  # speed
+        self.X = 0.  # global position
+        self.Y = 0.  # global position
+        self.yaw = 0.  # yaw angle
+        # key assumption: velocity is in the direction of where both the front
+        # and rear wheels are angled
+        self.v = 0.  # velocity
         self.delta_f = 0.  # wheel angle of front axle
         self.delta_r = 0.  # wheel angle of front axle
         self.beta = 0.  # slip angle
 
-    def set_state(self, x, y, yaw):
+    def set_state(self, X, Y, yaw):
         """
         set global position and orientation
         """
-        self.x = x
-        self.y = y
+        self.X = X
+        self.Y = Y
         self.yaw = yaw
 
     def update_state(self, v, delta_f, delta_r, dt):
@@ -42,8 +44,8 @@ class KinematicBicycle:
         yaw_rate = v * math.cos(self.beta) / (self.lf + self.lr) \
             * (math.tan(delta_f) - math.tan(delta_r))
 
-        self.x += v * math.cos(self.yaw + self.beta) * dt
-        self.y += v * math.sin(self.yaw + self.beta) * dt
+        self.X += v * math.cos(self.yaw + self.beta) * dt
+        self.Y += v * math.sin(self.yaw + self.beta) * dt
 
         self.yaw += yaw_rate * dt
         self.yaw = clip_to_pi(self.yaw)  # [-pi, pi]
@@ -63,12 +65,12 @@ class FrontWheelSteeringBicycle(KinematicBicycle):
     - 0 rear tire steering angle
     """
 
-    def update_state(self, v, steering_angle, dt):
+    def update_state(self, v, delta, dt):
         """
         same update calculation and KinematicBicycle but rear wheel steering
         angle will always be set to 0
         """
-        super().update_state(v, steering_angle, 0., dt)
+        super().update_state(v, delta, 0., dt)
 
 
 class CTRVBicycle(FrontWheelSteeringBicycle):
@@ -78,27 +80,27 @@ class CTRVBicycle(FrontWheelSteeringBicycle):
     - same as FrontWheelSteeringBicycle
     """
 
-    def update_state(self, v, steering_angle, dt):
+    def update_state(self, v, delta, dt):
         """
         CTRV update calculation incorporates the yaw rate in the position
         update of the KinematicBicycle, making it more accurate as the turn
         becomes sharper
         """
-        yaw_rate = v / (self.lf + self.lr) * math.tan(steering_angle)
+        yaw_rate = v / (self.lf + self.lr) * math.tan(delta)
 
         # only consider yaw rate in position update if it is nonzero
         if abs(yaw_rate) > 0.01:
-            self.x += v / yaw_rate * (math.sin(self.yaw + yaw_rate * dt)
+            self.X += v / yaw_rate * (math.sin(self.yaw + yaw_rate * dt)
                                       - math.sin(self.yaw))
-            self.y += v / yaw_rate * (math.cos(self.yaw)
+            self.Y += v / yaw_rate * (math.cos(self.yaw)
                                       - math.cos(self.yaw + yaw_rate * dt))
 
             self.yaw += yaw_rate * dt
             self.yaw = clip_to_pi(self.yaw)  # [-pi, pi]
 
             self.v = v
-            self.delta_f = steering_angle
+            self.delta_f = delta
         else:
             # if yaw rate is sufficiently small then just use normal kinematic
             # bicycle model for updating state
-            super().update_state(v, steering_angle, 0., dt)
+            super().update_state(v, delta, 0., dt)
