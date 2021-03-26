1fb18e3257ef3813d1872d3ad1389df026e51659
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-03-26 17:28:30 -0500

    using VehicleModel super class

--- dynamic_models.py
+++ dynamic_models.py
@@ -2,13 +2,16 @@ from math import sin, cos
 import numpy as np
 
 from lib.utils import clip_to_pi
+from model_templates import VehicleModel
 
 
-class DynamicBicycle:
+class DynamicBicycle(VehicleModel):
     """
     Canonical 2 Degrees of Freedom Dynamic Bicycle Model
+
+    Assumptions:
+    - zero aerodynamic friction
     """
-    g = 9.81  # gravitational constant m/s^2
 
     def __init__(self, lf, lr, Caf, Car, Iz, m):
         # model constants
@@ -20,15 +23,12 @@ class DynamicBicycle:
         self.m = m  # vehicle mass
 
         # dynamic parameters
-        self.X = 0. # global position
-        self.Y = 0.  # global position
-        self.yaw = 0.  # yaw angle
         self.yawd = 0.  # yaw rate
         self.vx = 0.  # longitudinal velocity
         self.vy = 0.  # lateral velocity
         self.delta = 0.  # wheel angle of front axle
 
-        # later state space
+        # state space
         # X = A*X + B*U
         # Y = C*X + D*U
         # X = [y, yd, yaw, yawd]  # where y = radius of curvature
@@ -40,23 +40,15 @@ class DynamicBicycle:
             [0, 0],
             [2*lf*Caf/Iz, 0],
         ])
-        self.C = np.eye(4)  # complete observability
-        self.D = np.zeros((4, 2))  # set feedthrough matrix to 0 as usual
 
-    def set_state(self, X, Y, yaw):
+    def update(self, accel, delta, dt):
         """
-        set global position and orientation
-        """
-        self.X = X
-        self.Y = Y
-        self.yaw = yaw
+        Update vehicle state
 
-    def update_state(self, delta, accel, dt):
-        """
-        update state given parameters:
-        - front wheel steering angle rate
-        - longitudinal acceleration
-        - timestep length
+        Input:
+        - accel: longitudinal acceleration
+        - delta: front wheel steering angle
+        - dt: timestep length
         assumes constant velocity
         """
         # model constants
@@ -86,6 +78,7 @@ class DynamicBicycle:
                 [0, -(2*lf*Caf-2*lr*Car)/(Iz*vx), 0, -(2*lf**2*Caf+2*lr**2*Car)/(Iz*vx)],
             ])
         else:
+            # at low speeds the dynamic bicycle model is unstable
             self.A = np.array([
                 [0, 1, 0, 0],
                 [0, 0, 0, 0],
@@ -105,3 +98,12 @@ class DynamicBicycle:
 
         self.X += (self.vx * cos(self.yaw) - self.vy * sin(self.yaw)) * dt
         self.Y += (self.vx * sin(self.yaw) + self.vy * cos(self.yaw)) * dt
+
+    def observe(self):
+        """
+        Return observable states
+
+        Output:
+        - (global x position, global y position, yaw angle)
+        """
+        return (self.X, self.Y, self.yaw)
--- kinematic_models.py
+++ kinematic_models.py
@@ -1,45 +1,40 @@
 import math
 
 from lib.utils import clip_to_pi
+from model_templates import VehicleModel
 
 
-class KinematicBicycle:
+class KinematicBicycle(VehicleModel):
     """
     Canonical Kinematic Bicycle Model
+
+    Assumptions:
+    - velocity at each wheel is in the direction of the wheel
     """
 
     def __init__(self, lf, lr):
         # model constants
         self.lf = lf  # distance from front axle to tracking point
-        self.lr = lr  # distance from rear aXle to tracking point
+        self.lr = lr  # distance from rear axle to tracking point
 
         # dynamic parameters
-        self.X = 0.  # global position
-        self.Y = 0.  # global position
-        self.yaw = 0.  # yaw angle
-        # key assumption: velocity is in the direction of where both the front
-        # and rear wheels are angled
+        self.beta = 0.  # slip angle
         self.v = 0.  # velocity
         self.delta_f = 0.  # wheel angle of front axle
         self.delta_r = 0.  # wheel angle of front axle
-        self.beta = 0.  # slip angle
 
-    def set_state(self, X, Y, yaw):
+    def update(self, v, delta_f, delta_r, dt):
         """
-        set global position and orientation
-        """
-        self.X = X
-        self.Y = Y
-        self.yaw = yaw
+        Update vehicle state
 
-    def update_state(self, v, delta_f, delta_r, dt):
-        """
-        update state given parameters:
-        - velocity
-        - front wheel steering angle
-        - rear wheel steering angle
-        - timestep length
-        assumes constant velocity
+        Input:
+        - v: velocity
+        - delta_f: front wheel steering angle
+        - delta_r: rear wheel steering angle
+        - dt: timestep length
+
+        Assumptions:
+        - zero acceleration
         """
         yaw_rate = v * math.cos(self.beta) / (self.lf + self.lr) \
             * (math.tan(delta_f) - math.tan(delta_r))
@@ -48,7 +43,7 @@ class KinematicBicycle:
         self.Y += v * math.sin(self.yaw + self.beta) * dt
 
         self.yaw += yaw_rate * dt
-        self.yaw = clip_to_pi(self.yaw)  # [-pi, pi]
+        self.yaw = clip_to_pi(self.yaw)
 
         self.v = v
         self.delta_f = delta_f
@@ -57,30 +52,44 @@ class KinematicBicycle:
             self.lf * math.tan(delta_r) + self.lr * math.tan(delta_f),
             self.lf + self.lr)
 
+    def observe(self):
+        """
+        Return observable states
+
+        Output:
+        - [global x position, global y position, yaw angle]
+        """
+        return (self.X, self.Y, self.yaw)
+
 
 class FrontWheelSteeringBicycle(KinematicBicycle):
     """
+    Only the front axle is used to steer
+
     Assumptions:
+    - same as parent
     - 0 slip angle
     - 0 rear tire steering angle
     """
 
-    def update_state(self, v, delta, dt):
+    def update(self, v, delta, dt):
         """
-        same update calculation and KinematicBicycle but rear wheel steering
+        Same update calculation as KinematicBicycle but rear wheel steering
         angle will always be set to 0
         """
-        super().update_state(v, delta, 0., dt)
+        super().update(v, delta, 0., dt)
 
 
 class CTRVBicycle(FrontWheelSteeringBicycle):
     """
     Constant Turn Rate and Velocity
+
     Assumptions:
-    - same as FrontWheelSteeringBicycle
+    - same as parent
+    - constant yaw rate and velocity during updates
     """
 
-    def update_state(self, v, delta, dt):
+    def update(self, v, delta, dt):
         """
         CTRV update calculation incorporates the yaw rate in the position
         update of the KinematicBicycle, making it more accurate as the turn
@@ -96,11 +105,11 @@ class CTRVBicycle(FrontWheelSteeringBicycle):
                                       - math.cos(self.yaw + yaw_rate * dt))
 
             self.yaw += yaw_rate * dt
-            self.yaw = clip_to_pi(self.yaw)  # [-pi, pi]
+            self.yaw = clip_to_pi(self.yaw)
 
             self.v = v
             self.delta_f = delta
         else:
             # if yaw rate is sufficiently small then just use normal kinematic
             # bicycle model for updating state
-            super().update_state(v, delta, 0., dt)
+            super().update(v, delta, 0., dt)
