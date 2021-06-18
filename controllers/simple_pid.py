22287fb4fe4c922f00fcefd395335264e7075ef8
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-06-18 10:59:24 -0500

    passing in dt instead of calculating it

--- controllers/simple_pid.py
+++ controllers/simple_pid.py
@@ -8,15 +8,13 @@ class SimplePID:
         self.Ki = Ki
         self.Kd = Kd
         self.error_integral = 0
-        self.last_error = 0
-        self.t = 0
+        self.last_error = None
 
-    def get_control_input(self, error):
-        curr_t = time.time()
-        dt = curr_t - self.t
+    def get_control_input(self, error, dt):
         self.error_integral += error
-        error_derivative = (error - self.last_error) / dt
-        self.t = curr_t
+        error_derivative = (error - self.last_error) / dt \
+            if self.last_error is not None \
+            else 0.
         self.last_error = error
         return self.Kp * error + self.Ki * self.error_integral \
             + self.Kd * error_derivative
