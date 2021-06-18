59f6060dac99b04dd117a423167244e42b68c230
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-06-18 10:59:48 -0500

    added euclidean distance and transformation function

--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 def get_wheel_angle(curvature, wheel_base):
@@ -20,3 +21,17 @@ def clip_to_pi(theta):
     restrict theta to [-pi, pi]
     '''
     return math.atan2(math.sin(theta), math.cos(theta))
+
+
+def euclidean_distance(a, b):
+    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+
+def rotate_and_translate(x: np.ndarray, theta: float, translation=np.zeros(2)):
+    '''
+    x: (N, 2)
+    '''
+    return x @ np.array([
+        [math.cos(theta), math.sin(theta)],
+        [-math.sin(theta), math.cos(theta)],
+    ]) + translation
