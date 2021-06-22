3aa74c5662b866d81a98db8a27495aac218f6959
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-06-22 09:40:28 -0500

    properly using lateral distance relative to path

--- controllers/stanley.py
+++ controllers/stanley.py
@@ -1,12 +1,13 @@
 import sys
 import math
 from pathlib import Path
+import numpy as np
 
 root = str(Path(__file__).parents[1].resolve())
 if root not in sys.path:
     sys.path.append(root)
 
-from lib.utils import clip_to_pi 
+from lib.utils import clip_to_pi, rotate_and_translate
 
 class StanleyController():
     """
@@ -19,7 +20,10 @@ class StanleyController():
 
     def steering_control(self, target_pos, target_yaw, veh_pos, veh_yaw, veh_speed):
         yaw_error = clip_to_pi(target_yaw - veh_yaw)
+        dx_global = target_pos[0] - veh_pos[0]
         dy_global = target_pos[1] - veh_pos[1]
+        _dx_local, dy_local = rotate_and_translate(
+            np.array([dx_global, dy_global]), -target_yaw)
 
         u = yaw_error + math.atan2(self.k * dy_global, veh_speed)
         return min(max(u, -self.delta_max), self.delta_max)
